d8892b3371602a1bfe349f099a58b5013a841f9a
Murakami <murakami.edson@gmail.com>
2023-06-02 15:43:22 -0300

    template base e free

--- src/main/java/com/epcafes/model/Role.java
+++ src/main/java/com/epcafes/model/Role.java
@@ -22,14 +22,18 @@ import lombok.Setter;
 @Entity
 @Table(name = "roles")
 public class Role {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@Column(nullable = false, unique = true)
 	private String name;
+	
 	@ManyToMany(mappedBy = "roles")
 	private List<User> users = new ArrayList<>();
 
+	
 	public Role(String name) {
 		this.name = name;
 	}
