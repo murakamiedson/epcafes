872bb45971789f59d99697af6dd236d64b232472
BomberBeetle <gdreks@gmail.com>
2023-10-02 10:58:49 -0300

    troca para jparepository

--- src/main/java/com/epcafes/repository/DespesaInsumoServicoRepository.java
+++ src/main/java/com/epcafes/repository/DespesaInsumoServicoRepository.java
@@ -1,11 +1,11 @@
 package com.epcafes.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 import com.epcafes.model.DespesaInsumoServico;
 
-public interface DespesaInsumoServicoRepository extends CrudRepository<DespesaInsumoServico, Long>{
+public interface DespesaInsumoServicoRepository extends JpaRepository<DespesaInsumoServico, Long>{
 	public List<DespesaInsumoServico> findByTenantId(Long tenantId);
 }
