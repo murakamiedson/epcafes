2104f1364df5a47b8137da4b8574ab6f6a78c338
gaianfree <murakami.edson@gaian.com.br>
2023-10-04 08:15:00 -0300

    adequando exemplo da arquitetura s

--- src/main/java/com/epcafes/EpCafesApplication.java
+++ src/main/java/com/epcafes/EpCafesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EpCafeApplication {
+public class EpCafesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EpCafeApplication.class, args);
+		SpringApplication.run(EpCafesApplication.class, args);
 	}
 
 }
