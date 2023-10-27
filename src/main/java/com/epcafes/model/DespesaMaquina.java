6422ef2cb1517841ccf7239a6a22d2c8074aea10
Isabella Bicudo de Souza <isabellabicudo@hotmail.com>
2023-10-26 22:01:45 -0300

    relatorioDespesaMaquina
    1 versao

--- src/main/java/com/epcafes/model/DespesaMaquina.java
+++ src/main/java/com/epcafes/model/DespesaMaquina.java
@@ -3,8 +3,13 @@ package com.epcafes.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.hibernate.annotations.NamedNativeQuery;
+
+import com.epcafes.dto.DespesaDTO;
 import com.epcafes.enums.FatorPotencia;
 
+import jakarta.persistence.ColumnResult;
+import jakarta.persistence.ConstructorResult;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -12,12 +17,38 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SqlResultSetMapping;
 import lombok.Data;
 
 @Data
+// @NamedQueries({
+// @NamedQuery(name = "DespesaMaquina.buscarDespesasDTO", query = "SELECT new
+// com.epcafes.dto.DespesaDTO( "
+// + "d.mesAno, "
+// + "d.valorTotal, "
+// + "m.id, "
+// + "m.nome, "
+// + "m.tipoCombustivel) "
+// + "FROM DespesaMaquina d "
+// + " INNER JOIN Maquina m on d.maquina = m.id "
+// + "WHERE "
+// + " mesAno <= :mesAno "
+// + "ORDER BY m.id"),
+// })
+
+@NamedNativeQuery(name = "DespesaMaquina.buscarDespesasDTO", 
+    query="select d.mes_ano as mesAno, d.valor_total as valorTotal, m.id as maquinaId, m.nome as maquinaNome, m.tipo_combustivel as tipoCombustivel from despesa_maquina d inner join maquina m on (d.maquina_id=m.id) order by m.id",
+    resultSetMapping = "Mapping.DespesasDTO")
+@SqlResultSetMapping(name = "Mapping.DespesasDTO",
+    classes = @ConstructorResult(targetClass = DespesaDTO.class,
+                                    columns = {@ColumnResult(name = "mesAno"),
+                                                @ColumnResult(name = "valorTotal"),
+                                                @ColumnResult(name = "maquinaId"),
+                                                @ColumnResult(name = "maquinaNome"),
+                                                @ColumnResult(name = "tipoCombustivel")}))
 @Entity
 public class DespesaMaquina {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -27,7 +58,7 @@ public class DespesaMaquina {
     private LocalDate mesAno;
 
     private BigDecimal horasTrabalhadas = new BigDecimal(0);
-    
+
     @Enumerated(EnumType.STRING)
     private FatorPotencia fatorPotencia;
 
