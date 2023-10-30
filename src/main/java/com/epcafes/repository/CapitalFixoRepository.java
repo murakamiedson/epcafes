3e7c266323db25d3392bdc8a15f7d5e497ad3290
unknown <pedroevs.0405@gmail.com>
2023-10-30 08:50:08 -0300

    Refeito

---
+++ src/main/java/com/epcafes/repository/CapitalFixoRepository.java
@@ -0,0 +1,9 @@
+package com.epcafes.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.epcafes.model.CapitalFixo;
+
+public interface CapitalFixoRepository extends JpaRepository<CapitalFixo, Long>  {
+
+}
