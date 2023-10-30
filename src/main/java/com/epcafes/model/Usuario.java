ee79a4e4dfc67f7b80d62e3b2d10ca50ff198fc9
BryanRibeiroBento <bryanribeiro.bento@gmail.com>
2023-10-30 14:59:49 -0300

    Criação do tenand_id

--- src/main/java/com/epcafes/model/Usuario.java
+++ src/main/java/com/epcafes/model/Usuario.java
@@ -22,17 +22,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @SuppressWarnings("serial")
-@Table(name="users")
-@Entity(name="users")
+@Table(name = "users")
+@Entity(name = "users")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario implements UserDetails{
-	
+public class Usuario implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
@@ -40,9 +39,9 @@ public class Usuario implements UserDetails{
     private String password;
     @Enumerated(EnumType.STRING)
     private UsuarioRole role;
-    private long tenant_id = 1;
+    private long tenant_id;
 
-    public Usuario(String login, String password, UsuarioRole role, long tenant_id){
+    public Usuario(String login, String password, UsuarioRole role, long tenant_id) {
         this.login = login;
         this.password = password;
         this.role = role;
@@ -51,29 +50,39 @@ public class Usuario implements UserDetails{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.role == UsuarioRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_GESTOR"), new SimpleGrantedAuthority("ROLE_TECNICO"));
-        else if (this.role == UsuarioRole.GESTOR) return List.of(new SimpleGrantedAuthority("ROLE_GESTOR"), new SimpleGrantedAuthority("ROLE_TECNICO"));
-        else return List.of(new SimpleGrantedAuthority("ROLE_TECNICO"));
+        if (this.role == UsuarioRole.ADMIN)
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_GESTOR"),
+                    new SimpleGrantedAuthority("ROLE_TECNICO"));
+        else if (this.role == UsuarioRole.GESTOR)
+            return List.of(new SimpleGrantedAuthority("ROLE_GESTOR"), new SimpleGrantedAuthority("ROLE_TECNICO"));
+        else
+            return List.of(new SimpleGrantedAuthority("ROLE_TECNICO"));
     }
+
     @Override
     public String getUsername() {
         return login;
     }
-    public long getTenant_id(){
+
+    public long getTenant_id() {
         return tenant_id;
     }
+
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
+
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
+
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
+
     @Override
     public boolean isEnabled() {
         return true;
