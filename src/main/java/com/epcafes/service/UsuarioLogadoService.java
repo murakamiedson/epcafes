4f71369071679bf2f5a629caee99126d30c22328
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-11-01 08:51:59 -0300

    Nome do Tenant adicionado à lista de usuários logados

--- src/main/java/com/epcafes/service/UsuarioLogadoService.java
+++ src/main/java/com/epcafes/service/UsuarioLogadoService.java
@@ -16,6 +16,9 @@ public class UsuarioLogadoService {
     @Autowired
     private SessionRegistry sessionRegistry;
 
+    @Autowired
+    private TenantService tenantService;
+
     public List<UsuarioLogadoDTO> getUsuariosLogados() {
         List<UsuarioLogadoDTO> usuariosLogados = new ArrayList<>();
 
@@ -23,12 +26,14 @@ public class UsuarioLogadoService {
 
         for (Object principal : sessionRegistry.getAllPrincipals()) {
             Usuario usuario = (Usuario)principal;
+            sessionRegistry.getAllSessions(principal, false).get(0);
             usuariosLogados.add(new UsuarioLogadoDTO(
                 codigo,
                 usuario.getLogin(),
                 usuario.getLogin(),
                 usuario.getRole(),
-                sessionRegistry.getAllSessions(principal, false).get(0).getLastRequest()));
+                sessionRegistry.getAllSessions(principal, false).get(0).getLastRequest(),
+                tenantService.loadTenantById(usuario.getTenant().getId()).getNome()));
             
             codigo += 1;
         }
