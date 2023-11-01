c028361ff17063d8b5584f456d59ef227a2849a7
gaianfree <murakami.edson@gaian.com.br>
2023-11-01 07:12:10 -0300

    retirados os warnnings

--- src/main/java/com/epcafes/model/Tenant.java
+++ src/main/java/com/epcafes/model/Tenant.java
@@ -14,7 +14,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@SuppressWarnings("serial")
 @Entity
 @Getter
 @Setter
--- src/main/java/com/epcafes/model/Usuario.java
+++ src/main/java/com/epcafes/model/Usuario.java
@@ -9,16 +9,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import com.epcafes.enums.UsuarioRole;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
