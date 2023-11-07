9e3e9b715e8a930826c219d96a61ad2904e59a1e
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-11-07 20:11:21 -0300

    import desnecessário removido

--- src/main/java/com/epcafes/controller/UsuarioLogadoController.java
+++ src/main/java/com/epcafes/controller/UsuarioLogadoController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.epcafes.exception.UsuarioLogadoException;
 import com.epcafes.service.UsuarioLogadoService;
 
 import lombok.extern.java.Log;
