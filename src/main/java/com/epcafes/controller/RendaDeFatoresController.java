7a0e6d94daa831c5cd9e3ef65b4c3db652944b4a
unknown <pedroevs.0405@gmail.com>
2023-11-09 08:20:09 -0300

    Menu Implementado

--- src/main/java/com/epcafes/controller/RendaDeFatoresController.java
+++ src/main/java/com/epcafes/controller/RendaDeFatoresController.java
@@ -2,16 +2,12 @@ package com.epcafes.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
-@RequestMapping("/rendaFatores")
 public class RendaDeFatoresController {
-	
-	@GetMapping("/capitalFixo")
-    public String capitalFixo() {
-    	    	
-        return "restricted/custo/CapitalFixo";
+
+	@GetMapping("/rendaFatores")
+	public String fazerLogin(){
+		return "restricted/custo/RendaFatores";
     }
 }
