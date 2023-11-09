62778a50436876c7235dfa3a0156048a7fa2b729
BomberBeetle <gdreks@gmail.com>
2023-11-09 08:34:59 -0300

    add generated serialversionuid to despesasoutrosinsumoservicoexception

--- src/main/java/com/epcafes/exception/DespesaOutrosInsumoServicoException.java
+++ src/main/java/com/epcafes/exception/DespesaOutrosInsumoServicoException.java
@@ -2,10 +2,12 @@ package com.epcafes.exception;
 
 public class DespesaOutrosInsumoServicoException extends TechnicalException{
 	
+	private static final long serialVersionUID = -7454825897059078491L;
+
 	private static String getExceptionCodeMessage(String code) {
 		if(code.equals("1337")) return "Nenhuma propriedade achada para o TenantID do usuário logado.";
 		else if(code.equals("413")) return "Id de despesa inexistente.";
-		else return ("Exceção em DespesaOutrosInsumoServico com código " + code);
+		else return ("Exceção de DespesaOutrosInsumoServico com código " + code);
 	}
 	
 	public DespesaOutrosInsumoServicoException(String code) {
