3e7c8600e16117748908cae18e4b4e8c37db2329
Murakami <murakami.edson@gmail.com>
2023-11-16 20:51:12 -0300

    inserido numero serial para as classes de exceção

--- src/main/java/com/epcafes/exception/BusinessException.java
+++ src/main/java/com/epcafes/exception/BusinessException.java
@@ -2,7 +2,9 @@ package com.epcafes.exception;
 
 public class BusinessException extends CustomException {
 
-    public BusinessException(String codigo, String mensagem) {
+	private static final long serialVersionUID = -5728330899236703196L;
+
+	public BusinessException(String codigo, String mensagem) {
         super(codigo, mensagem);
     }
 }
--- src/main/java/com/epcafes/exception/UsuarioLogadoException.java
+++ src/main/java/com/epcafes/exception/UsuarioLogadoException.java
@@ -1,7 +1,9 @@
 package com.epcafes.exception;
 
 public class UsuarioLogadoException extends TechnicalException {
-    public UsuarioLogadoException() {
+	private static final long serialVersionUID = 6869489007335953554L;
+
+	public UsuarioLogadoException() {
         super("123", "Não há usuários logados");
     }
 }
