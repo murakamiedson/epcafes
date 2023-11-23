1657591f10116bf4923cb45048fc5004cc88b9fa
gaianfree <murakami.edson@gaian.com.br>
2023-11-23 08:10:39 -0300

    import do EpCafeApplication

--- src/main/java/com/epcafes/EpCafesApplication.java
+++ src/main/java/com/epcafes/EpCafesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.epcafes.service.FilesStorageService;
+import com.epcafes.service.FileStorageService;
 
 @SpringBootApplication
 public class EpCafesApplication implements CommandLineRunner {
